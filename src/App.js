d523895d674f80d30789d08fe2816d67427ca2a9
bagasapk <bagasapk>
2021-10-22 09:36:50 +0700

    page donate

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Donate from "./pages/Donate";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={Register} />
+        <Route exact path="/" component={Register} />
         <Route path="/login" component={Login} />
+        <Route path="/donate" component={Donate} />
       </Router>
     </div>
   );
