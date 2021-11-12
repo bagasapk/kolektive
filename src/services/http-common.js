b8e517cac334289467d62bd25494e85e25ea911e
bagasapk <bagasapk>
2021-11-12 22:57:57 +0700

    fixing

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -4,8 +4,8 @@ axios.defaults.headers.common["Authorization"] =
   "Bearer " + localStorage.getItem("token");
 
 export default axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
-  // baseURL: "https://pacific-springs-44512.herokuapp.com/api",
+  // baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://pacific-springs-44512.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
